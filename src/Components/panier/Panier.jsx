6bfa8a0499714f70c76cf3db58ecdbeec4d93d5b
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-19 12:05:26 +0200

    evaluation conditionelle

--- src/Components/panier/Panier.jsx
+++ src/Components/panier/Panier.jsx
@@ -9,21 +9,27 @@ export default function Panier({panier, rendreArticle}) {
                 <h2 className="panierDiv1H2">Mon Panier:</h2>
             </div>
             <div className="panierDiv2">
-                <div className="panierDiv3">
-                    <img className="panierImg" src={Lookism} alt="lookism" />
-                    <p className="panierP">Manhwa de Looksim: {panier.lookism} </p>
-                    <button className="panierBtn" onClick={()=> rendreArticle("lookism")} disabled={panier.lookism ===0}>Rendre</button>
-                </div>
-                <div className="panierDiv3">
-                    <img className="panierImg" src={WB} alt="windbreaker" />
-                    <p className="panierP">Manhwa de WindBreaker:  {panier.windbreaker} </p>
-                    <button className="panierBtn" onClick={()=> rendreArticle("windbreaker")} disabled={panier.windbreaker ===0}>Rendre</button>
-                </div>
-                <div className="panierDiv3">
-                    <img className="panierImg" src={TOG} alt="tower of god" />
-                    <p className="panierP">Manhwa de TowerOfGod:  {panier.towerofgod} </p>
-                    <button className="panierBtn" onClick={()=> rendreArticle("towerofgod")} disabled={panier.towerofgod ===0}>Rendre</button>
-                </div>
+                {panier.lookism > 0 && (
+                    <div className="panierDiv3">
+                        <img className="panierImg" src={Lookism} alt="lookism" />
+                        <p className="panierP">Manhwa de Lookism: {panier.lookism}</p>
+                        <button className="panierBtn" onClick={() => rendreArticle("lookism")}>Rendre</button>
+                    </div>
+                )}
+                {panier.windbreaker > 0 &&(
+                    <div className="panierDiv3">
+                        <img className="panierImg" src={WB} alt="windbreaker" />
+                        <p className="panierP">Manhwa de WindBreaker:  {panier.windbreaker} </p>
+                        <button className="panierBtn" onClick={()=> rendreArticle("windbreaker")} >Rendre</button>
+                    </div>
+                )}
+                {panier.towerofgod > 0 &&(
+                    <div className="panierDiv3">
+                        <img className="panierImg" src={TOG} alt="tower of god" />
+                        <p className="panierP">Manhwa de TowerOfGod:  {panier.towerofgod} </p>
+                        <button className="panierBtn" onClick={()=> rendreArticle("towerofgod")} disabled={panier.towerofgod ===0}>Rendre</button>
+                    </div>
+                )}
             </div>
         </div>
         </>
