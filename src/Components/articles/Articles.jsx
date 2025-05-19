ac2cff3346ac85034507532d55707a2a6f559475
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-19 10:12:53 +0200

    creation card

--- src/Components/articles/Articles.jsx
+++ src/Components/articles/Articles.jsx
@@ -5,15 +5,42 @@ import TOG from "/src/assets/images/towerofgod.jpg"
 export default function Articles() {
     return(
         <>
-        <div>
-            <div>
-                <img className='articlesImg' src={Lookism} alt="" />
+        <div className='divArticles'>
+            <div className='articlesDiv1'>
+                <div className='articlesDivImg'>
+                    <img className='articlesImg' src={Lookism} alt="" />
+                </div>
+                
+                <div className='articlesDivText'>
+                    <h3>Lookism</h3>
+                    <p>Prix: €14,25</p>
+                    <p>Stock: 3</p>
+                    <button>Acheter</button>
+                </div>
             </div>
-            <div>
-                <img className='articlesImg' src={WB} alt="" />
+            <div className='articlesDiv1'>
+                <div className='articlesDivImg'>
+                    <img className='articlesImg' src={WB} alt="" />
+                </div>
+                
+                <div className='articlesDivText'>
+                    <h3>WindBreaker</h3>
+                    <p>Prix: €14,25</p>
+                    <p>Stock: 10</p>
+                    <button>Acheter</button>
+                </div>
             </div>
-            <div>
-                <img className='articlesImg' src={TOG} alt="" />
+            <div className='articlesDiv1'>
+                <div className='articlesDivImg'>
+                    <img className='articlesImg' src={TOG} alt="" />
+                </div>
+
+                <div className='articlesDivText'>
+                    <h3>Tower Of God</h3>
+                    <p>Prix: €14,25</p>
+                    <p>Stock: 6</p>
+                    <button>Acheter</button>
+                </div>
             </div>
         </div>
         
